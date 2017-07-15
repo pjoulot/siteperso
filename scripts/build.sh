525eed396194ece0c5903505577653b855f932fe
Philippe Joulot <philippe.joulot@laposte.net>
2017-07-15 19:34:08 +0200

    Add a build script to generate the website

---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BUILD_PATH="_site"
+
+if [ -d ".git" ]; then
+  jekyll build
+  
+  # Remove useless files
+  rm -f "$BUILD_PATH/README.md"
+  rm -Rf "$BUILD_PATH/scripts"
+  rm -f "$BUILD_PATH/feed.xml"
+else
+  echo "You must be in the root git folder to execute the build script."
+fi
