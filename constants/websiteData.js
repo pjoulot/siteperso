7d55d8f89019531a1b47e84daac4ea93208bdc15
Philippe Joulot <philippe.joulot@laposte.net>
2022-01-29 23:35:04 -0800

    Add reusable page template

---
+++ constants/websiteData.js
@@ -0,0 +1,15 @@
+const websiteData = {
+    defaultTitle: "Philippe Joulot - Developer, Web, Drupal",
+    defaultDescription: `Philippe Joulot, software/web developer`,
+    defaultImage: "/avatar.png",
+    meta: {
+        siteName: "Philippe Joulot",
+        twitterSite: "@stargayte",
+    },
+    websiteUrl: "https://philippejoulot.fr",
+    socialLinks: {
+      twitter: "https://twitter.com/stargayte",
+    },
+  };
+
+  export default websiteData;
