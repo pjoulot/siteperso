be10c166fb156ad38c74a2e464554b43575a7a06
Philippe Joulot <philippe.joulot@laposte.net>
2022-02-05 13:44:11 -0800

    Switch to Chakra UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,57 +1,20 @@
 import "../styles/globals.scss";
 
 import * as React from 'react';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
-export const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
-
-export default function MyApp({ Component, pageProps }) {
-  const [mode, setMode] = React.useState('light');
-  const colorMode = React.useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-      },
-    }),
-    [],
-  );
-
-  const theme = React.useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode,
-        },
-      }),
-    [mode],
-  );
-
-  // React.useEffect(() => {
-  //   console.log('passed');
-  //   localStorage.setItem("preferred-theme", mode);
-  // }, [mode]);
-
-  React.useEffect(() => {
-    const savedTheme = typeof window !== 'undefined' ? localStorage.getItem('preferred-theme') : null;
-    console.log(savedTheme);
-    if (savedTheme !== null) {
-      const defaultThemeValue = (savedTheme !== null) ? savedTheme : 'light';
-      localStorage.setItem("preferred-theme", defaultThemeValue);
-      setMode(defaultThemeValue);
-    }
-    else {
-      localStorage.setItem("preferred-theme", 'light');
-      setMode('light');
-    }
-  }, []);
-
+const colors = {
+  brand: {
+     900: "#1a365d",
+     800: "#153e75",
+     700: "#2a69ac",
+   },
+ }
+ const theme = extendTheme({ colors })
+export default function App({ Component, pageProps }) {
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+  </ChakraProvider>
   );
 }
